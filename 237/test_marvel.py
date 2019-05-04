1f0acdff50ef7b2d45d02636279d73c2a286bc97
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-04 18:45:49 +0200

    submission Bite 237 @ codechalleng.es

---
+++ 237/test_marvel.py
@@ -0,0 +1,17 @@
+from marvel import (most_popular_characters,
+                    max_and_min_years_new_characters,
+                    percentage_female)
+
+
+def test_most_popular_characters():
+    expected = ['Spider-Man', 'Captain America', 'Wolverine',
+                'Iron Man', 'Thor']
+    assert most_popular_characters() == expected
+
+
+def test_max_and_min_years_new_characters():
+    assert max_and_min_years_new_characters() == ('1993', '1958')
+
+
+def test_percentage_female():
+    assert percentage_female() == 23.43
