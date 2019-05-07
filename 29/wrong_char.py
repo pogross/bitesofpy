7e21c411b96287a21e2fc28df40205e4c170903d
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-07 22:57:12 +0200

    submission Bite 29 @ codechalleng.es

---
+++ 29/wrong_char.py
@@ -0,0 +1,18 @@
+ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+def get_index_different_char(chars: list) -> int:
+    alpha_count = 0
+    non_alpha_count = 0
+
+    for index, char in enumerate(chars):
+        if str(char) in ALPHANUMERIC and str(char) != "":
+            alpha_count = alpha_count + 1
+            alpha_index = index
+        else:
+            non_alpha_count = non_alpha_count + 1
+            non_index = index
+
+        if alpha_count > 1 and non_alpha_count == 1:
+            return non_index
+        elif non_alpha_count > 1 and alpha_count == 1:
+            return alpha_index
