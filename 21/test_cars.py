33fd86f98b83cb08964ca994272ca35fc66a03ff
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-14 22:32:49 +0200

    format

--- 21/test_cars.py
+++ 21/test_cars.py
@@ -1,9 +1,8 @@
-from cars import (get_all_jeeps, get_first_model_each_manufacturer,
-                  get_all_matching_models, sort_car_models)
+from cars import get_all_jeeps, get_first_model_each_manufacturer, get_all_matching_models, sort_car_models
 
 
 def test_get_all_jeeps():
-    expected = 'Grand Cherokee, Cherokee, Trailhawk, Trackhawk'
+    expected = "Grand Cherokee, Cherokee, Trailhawk, Trackhawk"
     actual = get_all_jeeps()
     assert type(actual) == str
     assert actual == expected
@@ -11,20 +10,20 @@ def test_get_all_jeeps():
 
 def test_get_first_model_each_manufacturer():
     actual = get_first_model_each_manufacturer()
-    expected = ['Falcon', 'Commodore', 'Maxima', 'Civic', 'Grand Cherokee']
+    expected = ["Falcon", "Commodore", "Maxima", "Civic", "Grand Cherokee"]
     assert actual == expected
 
 
 def test_get_all_matching_models_default_grep():
-    expected = ['Trailblazer', 'Trailhawk']
+    expected = ["Trailblazer", "Trailhawk"]
     # sorting requirement might be missed and is not essential for this method
     assert sorted(get_all_matching_models()) == expected
 
 
 def test_get_all_matching_models_different_grep():
-    expected = ['Accord', 'Commodore', 'Falcon']
+    expected = ["Accord", "Commodore", "Falcon"]
     # sorting requirement might be missed and is not essential for this method
-    assert sorted(get_all_matching_models(grep='CO')) == expected
+    assert sorted(get_all_matching_models(grep="CO")) == expected
 
 
 def test_sort_dict_alphabetically():
@@ -33,10 +32,10 @@ def test_sort_dict_alphabetically():
     # same keys and the same values.
     # The car models (values) need to be sorted here though
     expected = {
-        'Ford': ['Fairlane', 'Falcon', 'Festiva', 'Focus'],
-        'Holden': ['Barina', 'Captiva', 'Commodore', 'Trailblazer'],
-        'Honda': ['Accord', 'Civic', 'Jazz', 'Odyssey'],
-        'Jeep': ['Cherokee', 'Grand Cherokee', 'Trackhawk', 'Trailhawk'],
-        'Nissan': ['350Z', 'Maxima', 'Navara', 'Pulsar'],
+        "Ford": ["Fairlane", "Falcon", "Festiva", "Focus"],
+        "Holden": ["Barina", "Captiva", "Commodore", "Trailblazer"],
+        "Honda": ["Accord", "Civic", "Jazz", "Odyssey"],
+        "Jeep": ["Cherokee", "Grand Cherokee", "Trackhawk", "Trailhawk"],
+        "Nissan": ["350Z", "Maxima", "Navara", "Pulsar"],
     }
     assert actual == expected
