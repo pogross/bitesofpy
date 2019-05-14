cbb141178a7a29e4d3aff46cd0eb9853763eeb70
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-14 22:27:51 +0200

    submission Bite 21 @ codechalleng.es

---
+++ 21/cars.py
@@ -0,0 +1,29 @@
+cars = {
+    "Ford": ["Falcon", "Focus", "Festiva", "Fairlane"],
+    "Holden": ["Commodore", "Captiva", "Barina", "Trailblazer"],
+    "Nissan": ["Maxima", "Pulsar", "350Z", "Navara"],
+    "Honda": ["Civic", "Accord", "Odyssey", "Jazz"],
+    "Jeep": ["Grand Cherokee", "Cherokee", "Trailhawk", "Trackhawk"],
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models (original order)"""
+    return ", ".join(cars["Jeep"])
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    return [models[0] for models in cars.values()]
+
+
+def get_all_matching_models(cars=cars, grep="trail"):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    return [model for models in cars.values() for model in models if grep.lower() in model.lower()]
+
+
+def sort_car_models(cars=cars):
+    """sort the car models (values) and return the resulting cars dict"""
+    return {key: sorted(value) for key, value in cars.items()}
