6d85925fcbdf489135fef7882840ebfc60f2fa35
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-15 20:15:50 +0200

    submission Bite 197 @ codechalleng.es

---
+++ 197/mday.py
@@ -0,0 +1,13 @@
+from datetime import date
+import calendar
+
+
+def get_mothers_day_date(year):
+    """Given the passed in year int, return the date Mother's Day
+       is celebrated assuming it's the 2nd Sunday of May."""
+    c = calendar.Calendar(firstweekday=calendar.MONDAY)
+
+    second_week = c.monthdatescalendar(year, 5)[1]  # must be in the second week
+    mothers_day = [day for day in second_week if day.weekday() == 6][0]  # find sunday in the first week
+
+    return date(year, 5, mothers_day.day)
