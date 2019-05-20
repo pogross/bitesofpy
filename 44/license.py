b669d7abdc0333279e38a2df75a294085b66fdc3
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-20 16:09:20 +0200

    submission Bite 44 @ codechalleng.es

---
+++ 44/license.py
@@ -0,0 +1,9 @@
+from secrets import choice
+from string import ascii_uppercase, digits
+
+CHARS = list(ascii_uppercase + digits)
+
+
+def gen_key(parts=4, chars_per_part=8):
+    key_parts = ["".join([choice(CHARS) for char in range(chars_per_part)]) for part in range(parts)]
+    return "-".join(key_parts)
