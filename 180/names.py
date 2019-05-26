de6cd454fcda55dc5f48e75d32804169e2e41b34
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-05-26 13:22:01 +0200

    submission Bite 180 @ codechalleng.es

---
+++ 180/names.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from csv import DictReader
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    reader = DictReader(data.splitlines())
+    for row in reader:
+        countries[row["country_code"]].append(f"{row['first_name']} {row['last_name']}")
+    return countries
