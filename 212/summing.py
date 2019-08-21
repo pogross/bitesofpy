d4ebf4ee53b8128a9202e0361cbedabfed1c79d5
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-08-21 20:30:01 +0200

    submission Bite 212 @ codechalleng.es

---
+++ 212/summing.py
@@ -0,0 +1,18 @@
+from contextlib import suppress
+
+
+def sum_numbers(numbers):
+    """This generator divides each number by its consecutive number.
+       So if it gets passed in [4, 2, 1] it yields 4/2 and 2/1.
+       It ignores ZeroDivisionError and TypeError exceptions (latter happens
+       when a string or other non-numeric data type is in numbers)
+
+       Task: use contextlib's suppress twice to make the code below more concise.
+    """
+    for i, j in zip(numbers, numbers[1:]):
+        # replace the block below
+        with suppress(ZeroDivisionError):
+            yield i / j
+
+        with suppress(TypeError):
+            yield i / j
