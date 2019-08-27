fb47682f537153442cc1ed698404c477a948d446
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-08-27 17:10:38 +0200

    submission Bite 185 @ codechalleng.es

---
+++ 185/spelling.py
@@ -0,0 +1,25 @@
+from difflib import SequenceMatcher
+from os import path
+from urllib.request import urlretrieve
+
+
+DICTIONARY = path.join("tmp", "dictionary.txt")
+if not path.isfile(DICTIONARY):
+    urlretrieve("http://bit.ly/2iQ3dlZ", DICTIONARY)
+
+
+def load_words():
+    """Return a set of words from DICTIONARY"""
+    with open(DICTIONARY) as f:
+        return {word.strip().lower() for word in f.readlines()}
+
+
+def suggest_word(misspelled_word: str, words: set = None) -> str:
+    """Return a valid alternative word that best matches
+       the entered misspelled word"""
+    if words is None:
+        words = load_words()
+
+    likeness = [(word, SequenceMatcher(None, misspelled_word, word).ratio()) for word in words]
+
+    return max(likeness, key=lambda x: x[1])[0]
