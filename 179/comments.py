5311dd342919c740c751ca153f139fc18a4d591f
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-09-03 18:12:31 +0200

    submission Bite 179 @ codechalleng.es

---
+++ 179/comments.py
@@ -0,0 +1,12 @@
+import re
+
+
+def strip_comments(code: str):
+    # see Bite description
+    singleline_stripped = re.sub(r"^\s*#.*\n", "", code, flags=re.MULTILINE)
+    inline_stripped = re.sub(r"\s{2}# .*\n", "", singleline_stripped, flags=re.MULTILINE)
+    docstring_stripped = re.sub(
+        r"^\s*\"{3}[\s\S]*?\"{3}\n", "", inline_stripped, flags=re.MULTILINE
+    )
+
+    return docstring_stripped
