80dceb6fd3706de1f9106e713093febf6ede083f
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-09-26 19:41:42 +0200

    submission Bite 56 @ codechalleng.es

--- 56/bmi.py
+++ 56/bmi.py
@@ -13,7 +13,11 @@ def create_parser():
     """TODO:
        Create an ArgumentParser adding the right arguments to pass the tests,
        returns a argparse.ArgumentParser object"""
-    pass
+    parser = argparse.ArgumentParser(description="Calculate your BMI.")
+    parser.add_argument("-w", "--weight", metavar="WEIGTH", type=int, help="Your weight in kg")
+    parser.add_argument("-l", "--length", metavar="LENGTH", type=int, help="Your length in cm")
+
+    return parser
 
 
 def handle_args(args=None):
@@ -26,12 +30,12 @@ def handle_args(args=None):
 
     if args.weight and args.length:
         bmi = calc_bmi(args.weight, args.length)
-        print(f'Your BMI is: {bmi}')
+        print(f"Your BMI is: {bmi}")
     else:
         # could enforce SystemExit in create_parser/add_argument, but argparse
         # docs are not clear how to do it, so raising the exception here manually
         raise SystemExit
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     handle_args()
