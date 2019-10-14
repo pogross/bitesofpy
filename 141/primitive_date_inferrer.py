fb7a86d1218bfcd7a8bdd5aec331b85bb5b1385b
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-10-14 20:51:05 +0200

    submission Bite 141 @ codechalleng.es

---
+++ 141/primitive_date_inferrer.py
@@ -0,0 +1,88 @@
+from enum import Enum
+from datetime import datetime
+from collections import Counter
+
+
+class DateFormat(Enum):
+    DDMMYY = 0  # dd/mm/yy
+    MMDDYY = 1  # mm/dd/yy
+    YYMMDD = 2  # yy/mm/dd
+    NONPARSABLE = -999
+
+    @classmethod
+    def get_d_parse_formats(cls, val=None):
+        """ Arg:
+        val(int | None) enum member value
+        Returns:
+        1. for val=None a list of explicit format strings
+            for all supported date formats in this enum
+        2. for val=n an explicit format string for a given enum member value
+        """
+        d_parse_formats = ["%d/%m/%y", "%m/%d/%y", "%y/%m/%d"]
+        if val is None:
+            return d_parse_formats
+        if 0 <= val <= len(d_parse_formats):
+            return d_parse_formats[val]
+        raise ValueError
+
+
+class InfDateFmtError(Exception):
+    """custom exception when it is not possible to infer a date format
+    e.g. too many NONPARSABLE or a tie """
+
+    pass
+
+
+def _maybe_DateFormats(date_str):
+    """ Args:
+    date_str (str) string representing a date in unknown format
+    Returns:
+    a list of enum members, where each member represents
+    a possible date format for the input date_str
+    """
+    d_parse_formats = DateFormat.get_d_parse_formats()
+    maybe_formats = []
+    for idx, d_parse_fmt in enumerate(d_parse_formats):
+        try:
+            _parsed_date = datetime.strptime(
+                date_str, d_parse_fmt
+            )  # pylint: disable=W0612
+            maybe_formats.append(DateFormat(idx))
+        except ValueError:
+            pass
+    if len(maybe_formats) == 0:
+        maybe_formats.append(DateFormat.NONPARSABLE)
+    return maybe_formats
+
+
+def get_dates(dates):
+    """ Args:
+    dates (list) list of date strings
+    where each list item represents a date in unknown format
+    Returns:
+    list of date strings, where each list item represents
+    a date in yyyy-mm-dd format. Date format of input date strings is
+    inferred based on the most prevalent format in the dates list.
+    Alowed/supported date formats are defined in a DF enum class.
+    """
+    formats = []
+    for date in dates:
+        formats.extend(_maybe_DateFormats(date))
+    top_formats = Counter(formats).most_common(2)
+
+    if top_formats[0][1] == top_formats[1][1]:
+        raise InfDateFmtError
+    if top_formats[0][0] == DateFormat.NONPARSABLE:
+        raise InfDateFmtError
+
+    inferred_dates = []
+    for date in dates:
+        try:
+            inf_date = datetime.strptime(
+                date, DateFormat.get_d_parse_formats(top_formats[0][0].value)
+            )
+            inferred_dates.append(datetime.strftime(inf_date, "%Y-%m-%d"))
+        except ValueError:
+            inferred_dates.append("Invalid")
+
+    return inferred_dates
