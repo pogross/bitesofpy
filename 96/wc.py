293ae03df42f90eb1ae76f820aaf3a4629a096b1
Patrick-Oliver Groß <p.gross@tu-bs.de>
2019-10-16 21:15:58 +0200

    submission Bite 96 @ codechalleng.es

---
+++ 96/wc.py
@@ -0,0 +1,13 @@
+def wc(file_):
+    """Takes an absolute file path/name, calculates the number of
+       lines/words/chars, and returns a string of these numbers + file, e.g.:
+       3 12 60 /tmp/somefile
+       (both tabs and spaces are allowed as separator)"""
+    lines, words, chars = 0, 0, 0
+    with open(file_, "r") as f:
+        for line in f.readlines():
+            lines += 1
+            words += len(line.split())
+            chars += len(line)
+
+    return f"{lines} {words} {chars} {file_}"
