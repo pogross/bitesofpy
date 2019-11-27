cb9b36a32b3a93ec8d60ce9e2c5e28b750375550
Patrick-Oliver Groß <mail@pogross.de>
2019-11-27 22:30:18 +0100

    submission Bite 88 @ codechalleng.es

---
+++ 88/monitor.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from contextlib import contextmanager
+from datetime import date
+from time import time
+
+OPERATION_THRESHOLD_IN_SECONDS = 2.2
+ALERT_THRESHOLD = 3
+ALERT_MSG = "ALERT: suffering performance hit today"
+
+violations = Counter()
+
+
+def get_today():
+    """Making it easier to test/mock"""
+    return date.today()
+
+
+@contextmanager
+def timeit():
+    start = time()
+    yield
+    if time() - start >= OPERATION_THRESHOLD_IN_SECONDS:
+        violations[get_today()] += 1
+    if violations[get_today()] >= ALERT_THRESHOLD:
+        print(ALERT_MSG)
